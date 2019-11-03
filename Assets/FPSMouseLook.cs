78059f828ea2b9bb9c87077a58a28d8f2438fe39
richard <richardyu042@gmail.com>
2019-11-03 18:31:42 -0500

    inital commit

---
+++ Assets/FPSMouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMouseLook : MonoBehaviour
+{
+    //public enum RotationAxes { MouseXandY = 0, MouseX = 1, MouseY = 2};
+    //public RotationAxes axes = RotationAxes.MouseXandY;
+    [SerializeField]
+    public float sensitivity = 5;
+    [SerializeField]
+    public float smoothing = 2.0f;
+
+    public GameObject character;
+
+    private Vector2 look;
+
+    private Vector2 smoothv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var delta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        delta = Vector2.Scale(delta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothv.x = Mathf.Lerp(smoothv.x, delta.x, 1f / smoothing);
+        smoothv.y = Mathf.Lerp(smoothv.y, delta.y, 1f / smoothing);
+
+        look += smoothv;
+
+
+        transform.localRotation = Quaternion.AngleAxis(-look.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(look.x, character.transform.up);
+    }
+}
---
+++ Assets/fpsMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsMove : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpHeight = 5;
+    public float gravity = 20f;
+
+    private CharacterController cc;
+
+    private Vector3 move = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //jumping code
+        if (Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+        {
+            move.y = jumpHeight;           
+        }
+        //add gravity
+        move.y -= gravity * Time.deltaTime;
+
+        float strafe = Input.GetAxis("Horizontal");
+        float forward = Input.GetAxis("Vertical");
+
+        //moving front and back
+        move.x = speed * forward;
+        move.z = speed * strafe ;
+
+        cc.Move(transform.TransformDirection(move * Time.deltaTime));
+    }
+}
