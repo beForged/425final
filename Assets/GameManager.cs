517a7ab11e18a632cb706deac5075f518f67d07c
jugoodma <jugoodma@users.noreply.github.com>
2019-12-07 16:38:45 -0500

    music and gamemanager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class GameManager : MonoBehaviour {
+    public int[] collectibles = new int[5];
+    void Awake() {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameController");
+        // singleton pattern
+        if (objs.Length > 1) {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
