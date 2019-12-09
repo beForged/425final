d70db9e1944162180f647817e7dbbb67456fc825
Justin Goodman <jugoodma@users.noreply.github.com>
2019-12-09 02:39:06 -0500

    creating level, added aaaaa sound, dank memes

---
+++ Assets/LightShow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightShow : MonoBehaviour
+{
+    private int curr;
+    private int prev;
+    public float pulseRate = 5.0f;
+    public float rate = 0.25f;
+    private Color color = new Color(0f, 191.0f/255.0f, 115.0f/255.0f);
+    void Start()
+    {
+        InvokeRepeating("Pulse", 0.25f, pulseRate);
+    }
+
+    void Pulse() {
+        InvokeRepeating("NextLight", 1.0f, rate);
+    }
+    void NextLight() {
+        if (transform.childCount == 0)
+        {
+            return;
+        }
+        transform.GetChild(prev).GetComponentInChildren<Renderer>().material.SetColor("_EmissionColor", color * 0f);
+        if (curr == transform.childCount) {
+            prev = 0;
+            curr = 0;
+            CancelInvoke("NextLight");
+            return;
+        }
+        transform.GetChild(curr).GetComponentInChildren<Renderer>().material.SetColor("_EmissionColor", color);
+        prev = curr;
+        curr++;
+    }
+}
